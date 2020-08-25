a3d6959f563615640b678167b3184538a4a2adcd
pastukhVitalii <pastukh.v.7@gmail.com>
2020-08-25 21:55:30 +0300

    Updates

--- precache-manifest.a14f8a2c7516977de5fa08943dc9a919.js
+++ precache-manifest.a14f8a2c7516977de5fa08943dc9a919.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4b7d5b9ae35856c64053b995c2149260",
+    "revision": "19ce509218cc866e41fbf0e018f6e305",
     "url": "/todolist/index.html"
   },
   {
-    "revision": "66903948dc1f37401dd4",
+    "revision": "11a7a8a04951240cb255",
     "url": "/todolist/static/css/main.b76b620c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/todolist/static/js/2.cafbde88.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "66903948dc1f37401dd4",
-    "url": "/todolist/static/js/main.af059887.chunk.js"
+    "revision": "11a7a8a04951240cb255",
+    "url": "/todolist/static/js/main.3e448555.chunk.js"
   },
   {
     "revision": "49776f3ecba040d98291",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todolist/precache-manifest.cc8b9e002d40c88acb316bf58d743d2b.js"
+  "/todolist/precache-manifest.a14f8a2c7516977de5fa08943dc9a919.js"
 );
 
 self.addEventListener('message', (event) => {
