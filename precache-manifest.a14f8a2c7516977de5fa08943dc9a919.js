6ca966d04602f9f39a11871ae3c2ef3663f12997
pastukhVitalii <pastukh.v.7@gmail.com>
2020-08-25 14:18:55 +0300

    Updates

---
+++ precache-manifest.a14f8a2c7516977de5fa08943dc9a919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ce509218cc866e41fbf0e018f6e305",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "11a7a8a04951240cb255",
+    "url": "/todolist/static/css/main.b76b620c.chunk.css"
+  },
+  {
+    "revision": "e20c8825751a7e9ad0da",
+    "url": "/todolist/static/js/2.cafbde88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.cafbde88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a7a8a04951240cb255",
+    "url": "/todolist/static/js/main.3e448555.chunk.js"
+  },
+  {
+    "revision": "49776f3ecba040d98291",
+    "url": "/todolist/static/js/runtime-main.b4aa4502.js"
+  }
+]);
